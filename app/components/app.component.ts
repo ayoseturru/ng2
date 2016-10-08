28f91413d110be0be2c1fe34f71c7c925d463323
ayoseturru <ayose_gladiator@hotmail.com>
2016-10-08 18:19:12 +0100

    First commit

---
+++ app/components/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app/components/app.html'
+})
+
+export class AppComponent {
+    title: String = "Ng2ToNg"
+}
