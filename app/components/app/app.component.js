122aa618d781c76253277d159e980f56401b0c9a
ayoseturru <ayose_gladiator@hotmail.com>
2016-10-08 20:20:52 +0100

    Second step of tutorial

--- app/components/app/app.component.js
+++ app/components/app/app.component.js
@@ -8,19 +8,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
+var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = "Ng2ToNg";
+        this.title = 'Heroes app!';
+        this.heroes = HEROES;
     }
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'app',
-            templateUrl: 'app/components/app.html'
+            templateUrl: 'app/components/app/app.component.html'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 //# sourceMappingURL=app.component.js.map
---
+++ app/components/app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app/components/app/app.component.html'
+})
+
+export class AppComponent {
+    selectedHero: Hero;
+    title: string = 'Heroes app!';
+    heroes: Hero[] = HEROES;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+}
+
+export class Hero {
+    id: number;
+    name: string;
+}
+
+const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'},
+    {id: 15, name: 'Magneta'},
+    {id: 16, name: 'RubberMan'},
+    {id: 17, name: 'Dynama'},
+    {id: 18, name: 'Dr IQ'},
+    {id: 19, name: 'Magma'},
+    {id: 20, name: 'Tornado'}
+];
