d25e3fbacc47a46154f151921ff00518ea789509
ayoseturru <ayose_gladiator@hotmail.com>
2016-10-09 17:36:29 +0100

    HTTP with promises

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,7 +7,9 @@ import {AppComponent} from "./components/app/app.component";
 import {HeroService} from "./services/hero.service";
 import {RouterModule} from "@angular/router";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
-
+import {HttpModule} from "@angular/http";
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./services/in-memory-data.service";
 
 @NgModule({
     imports: [BrowserModule, FormsModule, RouterModule.forRoot([{
@@ -23,7 +25,7 @@ import {DashboardComponent} from "./components/dashboard/dashboard.component";
     }, {
         path: 'detail/:id',
         component: HeroDetailComponent
-    }])],
+    }]), HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService)],
     declarations: [HereosComponent, HeroDetailComponent, HereosComponent, AppComponent, DashboardComponent],
     providers: [HeroService],
     bootstrap: [AppComponent]
